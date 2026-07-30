49bed2b65e4f12156cf38f89027a329b1b14c19b
Jose <jose.casique@live.com>
2026-07-30 09:22:46 -0500

    Added feature to count lines matching log level provided by user

--- log_parser.py
+++ log_parser.py
@@ -4,11 +4,12 @@ import re
 # build the parser
 parser = argparse.ArgumentParser(description='Reads a log file and returns various error stats')
 parser.add_argument('--file', '-f', required=True, help='the log file to analyze')
+parser.add_argument('--level', '-l', help='ERROR|WARN|INFO')
 parser.add_argument('--version', '-v', action='version', version='%(prog)s v0.1')
 
 args = parser.parse_args()
 
-def line_counter(filename):
+def file_line_counter(filename):
     line_count=0
     with open(filename) as f:
         lines = f.readlines()
@@ -18,20 +19,27 @@ def line_counter(filename):
 
     return line_count
 
-def line_filter(filename):
+def log_level(filename, level=None):
+    if level is None:
+        level = 'error'
     with open(filename) as f:
         lines = f.readlines()
-        filter = "error"
         hit_count=0
         for line in lines:
-            if re.search(filter, line, re.IGNORECASE):
+            if re.search(level, line, re.IGNORECASE):
                 hit_count +=1
 
     return hit_count
 
 def main():
-    print(f"Total lines: {line_counter(args.file)}")
-    print(f"Errors: {line_filter(args.file)}")
+    total_lines = file_line_counter(args.file)
+
+    if args.level:
+        matching_line_count = log_level(args.file, args.level)
+        
+
+    print(f'Total lines in file: {total_lines}')
+    print(f'Lines matching log level {args.level.upper()}: {matching_line_count}')
     return 0
 
 
