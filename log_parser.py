48cc2a2559d26d5e0c9da5af43282e939f7b7313
Jose <jose.casique@live.com>
2026-07-31 05:56:27 -0500

    v0.2 - Consolidated functions and added filters

--- log_parser.py
+++ log_parser.py
@@ -4,42 +4,36 @@ import re
 # build the parser
 parser = argparse.ArgumentParser(description='Reads a log file and returns various error stats')
 parser.add_argument('--file', '-f', required=True, help='the log file to analyze')
-parser.add_argument('--level', '-l', help='ERROR|WARN|INFO')
-parser.add_argument('--version', '-v', action='version', version='%(prog)s v0.1')
+parser.add_argument('--level', '-l', help='ERROR|WARN|INFO. Default is ERROR')
+parser.add_argument('--version', '-v', action='version', version='%(prog)s v0.2')
 
 args = parser.parse_args()
 
-def file_line_counter(filename):
-    line_count=0
+def analyze_file(filename, level):
+    line_count = 0
+    hit_count = 0
+
     with open(filename) as f:
         lines = f.readlines()
-
-    for line in lines:
-        line_count +=1
-
-    return line_count
-
-def log_level(filename, level=None):
-    if level is None:
-        level = 'error'
-    with open(filename) as f:
-        lines = f.readlines()
-        hit_count=0
         for line in lines:
+            line_count += 1
             if re.search(level, line, re.IGNORECASE):
                 hit_count +=1
 
-    return hit_count
+    return line_count, hit_count
 
 def main():
-    total_lines = file_line_counter(args.file)
+    # If no level is provided the default is ERROR
+    log_level = 'error'
 
     if args.level:
-        matching_line_count = log_level(args.file, args.level)
+        log_level = args.level
+        matching_line_count = analyze_file(args.file, log_level)
         
+    total_lines, matching_line_count = analyze_file(args.file, log_level)
 
     print(f'Total lines in file: {total_lines}')
-    print(f'Lines matching log level {args.level.upper()}: {matching_line_count}')
+    print(f'Lines matching log level {log_level.upper()}: {matching_line_count}')
     return 0
 
 
