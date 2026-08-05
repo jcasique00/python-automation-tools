5551ac4a3c2cb3702022b341c1f2aa8fa285375e
Jose <jose.casique@live.com>
2026-08-05 09:07:27 -0500

    Updated version to v0.3 after new feature was added

--- log_parser.py
+++ log_parser.py
@@ -6,7 +6,7 @@ from collections import Counter
 parser = argparse.ArgumentParser(description='Reads a log file and returns various error stats')
 parser.add_argument('--file', '-f', required=True, help='the log file to analyze')
 parser.add_argument('--level', '-l', nargs='?', default='ERROR', const='ERROR', choices=['ERROR', 'WARN', 'INFO'], help='Default is ERROR if none is provided')
-parser.add_argument('--version', '-v', action='version', version='%(prog)s v0.2')
+parser.add_argument('--version', '-v', action='version', version='%(prog)s v0.3')
 parser.add_argument('--top5', '-t', action='store_true', help='Show the top 5 most frequent messages')
 
 args = parser.parse_args()
